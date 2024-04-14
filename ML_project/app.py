ff082569bf750b000d40f023af0402c3e42b7b70
Aditya Agre <adityaagrecollege@gmail.com>
2024-04-14 19:43:13 +0530

    Add files via upload

--- ML_project/app.py
+++ ML_project/app.py
@@ -6,7 +6,8 @@ from sklearn.linear_model import LogisticRegression
 app = Flask(__name__)
 
 # Load the dataset
-data = pd.read_csv(r"E:\ML_project\file1.csv")
+data = pd.read_csv(r"/Users/adityaagre/Downloads/ML_project/file1.csv")
+
 
 # Separate features and target variable
 y = data['Isfake']
@@ -17,19 +18,26 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 # Using Logistic regression
-clf = LogisticRegression(random_state=42)
-clf.fit(X_scaled, y)
+# clf = LogisticRegression(random_state=42)
+# clf.fit(X_scaled, y)
+
+## Using
+from sklearn.ensemble import RandomForestClassifier
+RF_object = RandomForestClassifier(random_state=42)
+RF_object.fit(X_scaled, y)
+
 
 # Define route for the index page
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 # Define route for prediction
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
-    
+
     # Convert input to array and scale
     input_features = [
         data["userFollowerCount"],
@@ -42,12 +50,17 @@ def predict():
         data["usernameLength"]
     ]
     input_scaled = scaler.transform([input_features])
-    
+
     # Make prediction
-    prediction = clf.predict(input_scaled)[0]
-    #result = "Real" if prediction == "Yes" else "Fake"
-    
+    prediction = RF_object.predict(input_scaled)[0]
+    if prediction == "Yes":
+        prediction = "Prediction: Account is most probably fake."
+    else:
+        prediction = "Prediction: Account is most probably genuine."
+    # result = "Real" if prediction == "Yes" else "Fake"
+
     return jsonify({"prediction": prediction})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
